24f8c7fbf9caeb3d5b7070dfc008968d241e0116
飞 <ilvxna@hotmail.com>
2016-09-22 13:33:05 +0800

    test false

--- tutorial01/test.c
+++ tutorial01/test.c
@@ -32,8 +32,15 @@ static void test_parse_true()
 	lept_value v;
 	v.type = LEPT_FALSE;
 	EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&v, "true"));
-	EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
+	EXPECT_EQ_INT(LEPT_TRUE, lept_get_type(&v));
 	
+}
+static void test_parse_false()
+{
+	lept_value v;
+	v.type = LEPT_FALSE;
+	EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&v, "false"));
+	EXPECT_EQ_INT(LEPT_FALSE, lept_get_type(&v));
 }
 static void test_parse_expect_value() {
     lept_value v;
