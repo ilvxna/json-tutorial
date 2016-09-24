9a8dff7149e3625ed6e9cf7cf6ff9bc704f5bf80
飞 <ilvxna@hotmail.com>
2016-09-24 10:58:57 +0800

    lept parse number

--- tutorial01/leptjson.c
+++ tutorial01/leptjson.c
@@ -33,6 +33,26 @@ static int lept_parse_true(lept_context* c, lept_value* v)
 	return LEPT_PARSE_OK;
 
 }
+static int lept_parse_literal(lept_context* c, lept_value* v, const char* literal, lept_type type)
+{
+	size_t i;
+	EXPECT(c, literal[0]);
+	for (i=0;literal[i+1];i++)
+	{
+		if (c->json[i] != literal[i + 1])
+			return LEPT_PARSE_INVALID_VALUE;
+	}
+	c->json += 1;
+	v->type = type;
+	return LEPT_PARSE_OK;
+}
+static int lept_parse_bumber(lept_context* c, lept_value* v)
+{
+	const char* p = c->json;
+	/*#### ¸ººÅ####*/
+
+
+}
 static int lept_parse_false(lept_context* c, lept_value* v)
 {
 	EXPECT(c, 'f');
